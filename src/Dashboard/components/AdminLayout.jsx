04150a55fc97e0d281fe967260622f45ed16d478
refat5 <mdrefat382139@gmail.com>
2023-08-04 02:10:48 +0600

    sidebar content add

--- src/Dashboard/components/AdminLayout.jsx
+++ src/Dashboard/components/AdminLayout.jsx
@@ -1,27 +1,25 @@
-import React from 'react';
 import '../Style/adminLayout.css';
+
 import NavbarComponent from './Navbar';
+import React from 'react';
 import Sidebar from './Sidebar';
 
 const AdminLayout = ({ children }) => {
-  return (
-    <div style={{ minHeight: '100vh' }}>
-      <NavbarComponent />
-      <div className='container-fluid' style={{ height: '100%' }}>
-        <div className='row' style={{ height: '100%' }}>
-          <div className='dashboard-layout'>
-            <Sidebar />
-            <main
-              className='col-md-9 ms-sm-auto col-lg-10'
-              style={{ padding: '20px' }}
-            >
-              {children}
-            </main>
-          </div>
+    return (
+        <div style={{ minHeight: '100vh' }}>
+            <NavbarComponent />
+            <div className="container-fluid" style={{ height: '100%' }}>
+                <div className="row" style={{ height: '100%' }}>
+                    <div className="dashboard-layout">
+                        <Sidebar />
+                        <main className="col-md-7 ms-sm-auto col-lg-7" style={{ padding: '20px' }}>
+                            {children}
+                        </main>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default AdminLayout;
--- src/Dashboard/components/Sidebar.jsx
+++ src/Dashboard/components/Sidebar.jsx
@@ -1,13 +1,88 @@
-import React from 'react';
 import '../Style/sidebar.css';
+import 'react-toggle/style.css';
+
+import React, { useState } from 'react'; // Import the useState hook
+import { faCog, faUserShield, faWallet } from '@fortawesome/free-solid-svg-icons';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Toggle from 'react-toggle';
 
 const Sidebar = () => {
-  return (
-    <div className='sidebar-container'>
-      {/* Add your sidebar content here */}
-      Sidebar
-    </div>
-  );
+    const [isToggleOn, setIsToggleOn] = useState(false);
+
+    const handleToggle = () => {
+        setIsToggleOn(!isToggleOn);
+    };
+    return (
+        <div className="sidebar-container mt-2 rounded">
+            <div className="row">
+                <div className="col-md-3">
+                    <FontAwesomeIcon
+                        icon={faUserShield}
+                        className="help-icon mt-2 ml-3 "
+                        color="gray"
+                        fontSize="25"
+                    />
+                </div>
+                <div className="col-md-9 pl-0">
+                    <span className="text-lg how-to-guide">How-to guide</span>
+                </div>
+            </div>
+            <div className="row mt-5">
+                <div className="col-md-3">
+                    <FontAwesomeIcon
+                        icon={faCog}
+                        className="help-icon mt-2 ml-3 "
+                        color="gray"
+                        fontSize="25"
+                    />
+                </div>
+                <div className="col-md-9 pl-0">
+                    <span className="text-lg how-to-guide">Settings</span>
+                    <div className="mt-3">
+                        <div className="">
+                            <label className="switch">
+                                <input
+                                    type="checkbox"
+                                    checked={isToggleOn}
+                                    onChange={handleToggle}
+                                />
+                                <span className="slider "></span>
+                            </label>
+                            <span className="ml-2 mode"> Simple mode </span>
+                        </div>
+                    </div>
+                    <div className="mt-3">
+                        <div className="">
+                            <label className="switch">
+                                <input
+                                    type="checkbox"
+                                    checked={isToggleOn}
+                                    onChange={handleToggle}
+                                />
+                                <span className="slider "></span>
+                            </label>
+                            <span className="ml-2 mode"> Advanced mode </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="row mt-4">
+                <div className="col-md-3">
+                    <FontAwesomeIcon
+                        icon={faWallet}
+                        className="help-icon mt-2 ml-3 "
+                        color="gray"
+                        fontSize="25"
+                    />
+                </div>
+                <div className="col-md-9 pl-0">
+                    <span className="text-lg how-to-guide">Log</span>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Sidebar;
