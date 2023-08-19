5dc2d4d02eaca1b4fa09f0f43e8bb5bca148eedb
Atik1000 <foysalkazi45@gmail.com>
2023-08-19 12:23:10 +0600

    not found page add

--- src/LandingPage/container/SignUp.jsx
+++ src/LandingPage/container/SignUp.jsx
@@ -1,36 +1,36 @@
-import '../style/landingpage/signup.css';
+import "../style/landingpage/signup.css";
 
-import { ReactComponent as ACOSICOn } from '../icon/ACoS-03.svg';
-import { Link } from 'react-router-dom';
-import React from 'react';
-import googleIcon from '../icon/googleIcon.webp';
+import { ReactComponent as ACOSICOn } from "../icon/ACoS-03.svg";
+import { Link } from "react-router-dom";
+import React from "react";
+import googleIcon from "../icon/googleIcon.webp";
 
 const SignUp = () => {
-    return (
-        <div className="signup">
-        <div className="container mb-5">
-            <div className="d-flex justify-content-center">
-                <ACOSICOn className="icon-signup" style={{ height: 50 }} />
-            </div>
-            <div className="d-flex justify-content-center">
-                <div className="mb-4 pb-2  login-title">
-                    Log in or sign up to continue to ACoS.app
-                </div>
-            </div>
-            <div className="d-flex justify-content-center button-contenct">
-                <button className="btn btn-sm btn-primary mb-3  sign-up-btn">
-                    <img src={googleIcon} alt="google-icon" className="google-icon " />
-                    Continue with Google
-                </button>
-            </div>
-            <div className="d-flex justify-content-center">
-                <div className="login-bottom-title">
-                    By signing up you agree to <Link href="">Terms and Conditions</Link>{' '}
-                </div>
-            </div>
-            </div>
-            </div>
-    );
+  return (
+    <div className="signup">
+      <div className="container mb-5">
+        <div className="d-flex justify-content-center">
+          <ACOSICOn className="icon-signup" style={{ height: 50 }} />
+        </div>
+        <div className="d-flex justify-content-center">
+          <div className="mb-4 pb-2  login-title">
+            Log in or sign up to continue to ACoS.app
+          </div>
+        </div>
+        <div className="d-flex justify-content-center button-contenct">
+          <button className="btn btn-sm btn-primary mb-3  sign-up-btn">
+            <img src={googleIcon} alt="google-icon" className="google-icon " />
+            Continue with Google
+          </button>
+        </div>
+        <div className="d-flex justify-content-center">
+          <div className="login-bottom-title">
+            By signing up you agree to <Link href="">Terms and Conditions</Link>{" "}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
--- src/LandingPage/container/notfound.jsx
+++ src/LandingPage/container/notfound.jsx
@@ -1,13 +1,81 @@
 // NotFound.js (Optional)
-import React from 'react';
+import React from "react";
+import "../style/landingpage/notfound.css";
+import oops from "../icon/oops.png";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faUserGroup } from "@fortawesome/free-solid-svg-icons";
 
 const NotFound = () => {
-    return (
-        <div>
-            <h2>404 - Not Found</h2>
-            <p>The page you are looking for does not exist.</p>
+  return (
+    <div>
+      <div className="top-area">
+        <div className="d-flex justify-content-center">
+          <img src={oops} alt="google-icon" />
         </div>
-    );
+
+        <h2
+          className="text-center"
+          style={{ color: "white", fontWeight: "bold", fontSize: "60px" }}
+        >
+          Opus Pro
+        </h2>
+        <p
+          className="text-center"
+          style={{ color: "white", fontWeight: "bold", fontSize: "40px" }}
+        >
+          Oops!, something went wrong
+        </p>
+      </div>
+      <div className="mid-area">
+        <p
+          className="text-center"
+          style={{ fontSize: "20px", fontWeight: "normal" }}
+        >
+          There could be a misconfiguration in the system or a service outage.
+          We track these errors automatically,but if the problem persists feel
+          free to
+        </p>
+        <p
+          className="text-center"
+          style={{ fontSize: "20px", fontWeight: "normal" }}
+        >
+          Contact us.
+        </p>
+        <p
+          className="text-center"
+          style={{ fontSize: "20px", fontWeight: "normal" }}
+        >
+          Please try again.
+        </p>
+      </div>
+      <div className="container">
+        <div className="last-area">
+          <div className="row">
+            <div className="col">
+              <p>TECHNICAL DETAILS</p>
+              <Link style={{ textDecoration: "none" }}>
+                See details for this error
+              </Link>
+            </div>
+            <div className="col">
+              <p>SUPPORT</p>
+              <Link style={{ textDecoration: "none" }}>
+                <FontAwesomeIcon icon={faUserGroup} className="mr-4" />
+                Get Help
+              </Link>
+              <br />
+              <br />
+              <Link style={{ textDecoration: "none" }}>
+                <FontAwesomeIcon icon={faEnvelope} className="mr-4" />
+                login-support@opus.pro
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
