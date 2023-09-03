0b5a855b5ec41fc00357afe71c7b5640fb1d46be
refat5 <mdrefat382139@gmail.com>
2023-09-03 13:15:36 +0600

    issue fixed

--- src/Dashboard/components/Sidebar.jsx
+++ src/Dashboard/components/Sidebar.jsx
@@ -22,79 +22,112 @@ const Sidebar = () => {
     };
 
     return (
-        <div className="sidebar-container mt-2 rounded">
-            <div className="row">
-                <div className="col-md-3">
-                    <FontAwesomeIcon
-                        icon={faUserShield}
-                        className="help-icon mt-2 ml-3 "
-                        color="gray"
-                        fontSize="25"
-                    />
-                </div>
-                <div className="col-md-9 pl-0">
-                    <span className="text-lg how-to-guide-admin">How-to guide</span>
-                </div>
-            </div>
-            <div className="row mt-5">
-                <div className="col-md-3">
-                    <FontAwesomeIcon
-                        icon={faCog}
-                        className="help-icon mt-2 ml-3 "
-                        color="gray"
-                        fontSize="25"
-                    />
-                </div>
-                <div className="col-md-9 pl-0">
-                    <span className="text-lg how-to-guide-admin">Settings</span>
-                    <div className="mt-3">
-                        <div className="">
-                            <label className={`switch ${isFirstToggleOn ? 'red' : 'blue'}`}>
-                                <input
-                                    type="checkbox"
-                                    checked={isFirstToggleOn}
-                                    onChange={handleFirstToggle}
-                                />
-                                <span
-                                    className={`slider ${isFirstToggleOn ? 'red' : 'blue'}`}></span>
-                            </label>
-                            <span className="ml-2 mode"> Simple mode </span>
-                        </div>
-                    </div>
-                    <div className="mt-3">
-                        <div className="">
-                            <label className={`switch ${isSecondToggleOn ? 'blue' : 'red'}`}>
-                                <input
-                                    type="checkbox"
-                                    checked={isSecondToggleOn}
-                                    onChange={handleSecondToggle}
-                                />
-                                <span
-                                    className={`slider ${
-                                        isSecondToggleOn ? 'blue' : 'red'
-                                    }`}></span>
-                            </label>
-                            <span className="ml-2 mode"> Advanced Mode </span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="row mt-4">
-                <div className="col-md-3">
-                    <FontAwesomeIcon
-                        icon={faWallet}
-                        className="help-icon mt-2 ml-3 "
-                        color="gray"
-                        fontSize="25"
-                    />
-                </div>
-                <div className="col-md-9 pl-0">
-                    <Link to="logs">
-                        <span className="text-lg how-to-guide-admin">Logs</span>
-                    </Link>
-                </div>
+        <div>
+            <div className="sidebar-container mt-2 rounded">
                 <div className="row">
+                    <div className="col-md-3">
+                        <FontAwesomeIcon
+                            icon={faUserShield}
+                            className="help-icon mt-2 ml-3 "
+                            color="gray"
+                            fontSize="35"
+                        />
+                    </div>
+                    <div className="col-md-9 pl-0">
+                        <span className="text-lg how-to-guide-admin">How-to guide</span>
+                    </div>
+                </div>
+                <div className="row mt-5">
+                    <div className="col-md-3">
+                        <FontAwesomeIcon
+                            icon={faCog}
+                            className="help-icon mt-2 ml-3 "
+                            color="gray"
+                            fontSize="35"
+                        />
+                    </div>
+                    <div className="col-md-9 pl-0">
+                        <span className="text-lg how-to-guide-admin">Settings</span>
+                        <div className="mt-3">
+                            <div className="">
+                                <label className={`switch ${isFirstToggleOn ? 'red' : 'blue'}`}>
+                                    <input
+                                        type="checkbox"
+                                        checked={isFirstToggleOn}
+                                        onChange={handleFirstToggle}
+                                    />
+                                    <span
+                                        className={`slider ${
+                                            isFirstToggleOn ? 'red' : 'blue'
+                                        }`}></span>
+                                </label>
+                                <span className="ml-2 mode"> Simple mode </span>
+                            </div>
+                        </div>
+                        <div className="mt-3">
+                            <div className="">
+                                <label className={`switch ${isSecondToggleOn ? 'blue' : 'red'}`}>
+                                    <input
+                                        type="checkbox"
+                                        checked={isSecondToggleOn}
+                                        onChange={handleSecondToggle}
+                                    />
+                                    <span
+                                        className={`slider ${
+                                            isSecondToggleOn ? 'blue' : 'red'
+                                        }`}></span>
+                                </label>
+                                <span className="ml-2 mode"> Advanced Mode </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row mt-4">
+                    <div className="col-md-3">
+                        <FontAwesomeIcon
+                            icon={faWallet}
+                            className="help-icon mt-2 ml-3 "
+                            color="gray"
+                            fontSize="35"
+                        />
+                    </div>
+                    <div className="col-md-9 pl-0 ">
+                        <Link to="logs" className="logs">
+                            <span className="text-lg how-to-guide-admin">Logs</span>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            <div className="sidebar-container mt-2 rounded">
+                <div className="row mt-2">
+                    <div className="col-md-7 p-3 mb-4">
+                        <strong>Keyword & Targets Optimize</strong>
+                    </div>
+                    <div className="col-md-5 mt-2">
+                        <div class="dropdown">
+                            <button
+                                class="btn btn-outline-secondary dropdown-toggle btn-drp"
+                                type="button"
+                                id="dropdownMenuButton"
+                                data-toggle="dropdown"
+                                aria-haspopup="true"
+                                aria-expanded="false">
+                                Last 7 Days{' '}
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" href="#">
+                                    Action
+                                </a>
+                                <a class="dropdown-item" href="#">
+                                    Another action
+                                </a>
+                                <a class="dropdown-item" href="#">
+                                    Something else here
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                     <div className="col-md-6">
                         <ApexChart />
                     </div>
--- src/Dashboard/container/Home.jsx
+++ src/Dashboard/container/Home.jsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 
 import { FileUploader } from 'react-drag-drop-files';
 import fileIcon from '../../LandingPage/icon/foldericon.webp';
+
 const fileTypes = ['JSON', 'Xlsx', 'csv', 'Pdf'];
 
 const DashbordHome = () => {
@@ -30,15 +31,15 @@ const DashbordHome = () => {
     };
 
     return (
-        <div className="dashboard-containr">
-            <div className="container">
+        <div className="dashboard-containr p-0">
+            <div className="">
                 <div className="d-flex justify-content-center">
                     <div className="row">
                         <div className="col">
-                            <div className=" shadow-sm rounded p-3 upload-card">
-                                <div className="p-10 bg-white">
-                                    <div className="p3">
-                                        <div className="  text-center h2 mb-4">
+                            <div className=" shadow-sm   upload-card">
+                                <div className=" bg-white rounded">
+                                    <div className="p-3">
+                                        <div className="  text-center h3 mb-4">
                                             Upload Bulk File 1{' '}
                                             <span style={{ color: '#108ACC' }}>Yesterday</span>
                                         </div>
@@ -49,7 +50,7 @@ const DashbordHome = () => {
                                             types={fileTypes}>
                                             <div className="fileOne row  m-2">
                                                 <div className="col-md-4">
-                                                    <div className="folderIcon">
+                                                    <div className="folderIconAdmin">
                                                         <img
                                                             src={fileIcon}
                                                             alt="fileIcon"
@@ -59,7 +60,7 @@ const DashbordHome = () => {
                                                 </div>
                                                 <div className="col-md-8 mt-2">
                                                     <div className="file-title">FILE1 </div>
-                                                    <div className="file-sub">YESTERDAY </div>
+                                                    <div className="file-sub-admin">YESTERDAY </div>
                                                 </div>
                                                 <div className="col-md-12 mt-3 mb-5">
                                                     <div className="drag-drop mb-4">
@@ -90,10 +91,10 @@ const DashbordHome = () => {
                             </div>
                         </div>
                         <div className="col">
-                            <div className=" shadow-sm rounded p-3 upload-card">
-                                <div className="p-10 bg-white">
-                                    <div className="p3">
-                                        <div className="  text-center h2 mb-4">
+                            <div className=" shadow-sm  upload-card">
+                                <div className=" bg-white rounded">
+                                    <div className="p-3">
+                                        <div className="  text-center h3 mb-4">
                                             Upload Bulk File 2{' '}
                                             <span style={{ color: '#108ACC' }}>60 Days</span>
                                         </div>
@@ -104,7 +105,7 @@ const DashbordHome = () => {
                                             types={fileTypes}>
                                             <div className="fileOne row  m-2">
                                                 <div className="col-md-4">
-                                                    <div className="folderIcon">
+                                                    <div className="folderIconAdmin">
                                                         <img
                                                             src={fileIcon}
                                                             alt="fileIcon"
@@ -114,7 +115,7 @@ const DashbordHome = () => {
                                                 </div>
                                                 <div className="col-md-8 mt-2">
                                                     <div className="file-title">FILE 2 </div>
-                                                    <div className="file-sub">60 DAYS </div>
+                                                    <div className="file-sub-admin">60 DAYS </div>
                                                 </div>
                                                 <div className="col-md-12 mt-3 mb-5">
                                                     <div className="drag-drop mb-4">
